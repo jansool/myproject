18ebf12e5417607808e04dfd13c60d5eb6c3384a
jansool <jssolovinskaya@gmail.com>
2023-04-23 19:24:41 +0200

    при сохранении поста выдавало ошибку 'NoneType' object has no attribute 'date', добавила проверку

--- myapp/models.py
+++ myapp/models.py
@@ -13,7 +13,10 @@ class Topic(models.Model):
 
 class User(models.Model):
     name = models.CharField(max_length=50)
-    prefers = models.ManyToManyField(Topic, related_name="topic_followers")
+    prefers = models.ManyToManyField(Topic, related_name="topic_followers", blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Post(models.Model):
@@ -26,7 +29,11 @@ class Post(models.Model):
                                    validators=[MinLengthValidator(1, message="At least one topic is required.")])
 
     def save(self, *args, **kwargs):
-        self.slug = slugify(self.title + '-' + str(self.created_at.date()))
+        if self.created_at:
+            self.slug = slugify(self.title + '-' + str(self.created_at.date()))
+        else:
+            self.slug = slugify(self.title)
+
         super().save(*args, **kwargs)
 
     def __str__(self):
@@ -38,3 +45,4 @@ class Comment(models.Model):
     content = models.TextField()
     user = models.ForeignKey(User, related_name="user_comments", on_delete=models.CASCADE)
     post = models.ForeignKey(Post, related_name="post_comments", on_delete=models.CASCADE)
+
